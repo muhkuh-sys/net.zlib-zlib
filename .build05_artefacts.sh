70a6a9f2dcf8667d3802882c19ca8cead6676734
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-19 16:56:22 +0100

    Build a package for windows and ubuntu.

---
+++ .build05_artefacts.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+set -e
+
+#-----------------------------------------------------------------------------
+#
+# Build all artefacts.
+#
+pushd build
+rm -rf net.zlib-zlib
+rm -rf windows_x86
+rm -rf windows_x86_64
+rm -rf ubuntu_1204_x86
+rm -rf ubuntu_1204_x86_64
+rm -rf ubuntu_1404_x86
+rm -rf ubuntu_1404_x86_64
+rm -rf ubuntu_1604_x86
+rm -rf ubuntu_1604_x86_64
+rm -rf ubuntu_1610_x86
+rm -rf ubuntu_1610_x86_64
+
+mkdir net.zlib-zlib
+mkdir windows_x86
+mkdir windows_x86_64
+mkdir ubuntu_1204_x86
+mkdir ubuntu_1204_x86_64
+mkdir ubuntu_1404_x86
+mkdir ubuntu_1404_x86_64
+mkdir ubuntu_1604_x86
+mkdir ubuntu_1604_x86_64
+mkdir ubuntu_1610_x86
+mkdir ubuntu_1610_x86_64
+
+tar --extract --directory windows_x86 --file build_windows_x86.tar.gz --gzip
+tar --extract --directory windows_x86_64 --file build_windows_x86_64.tar.gz --gzip
+tar --extract --directory ubuntu_1204_x86 --file build_ubuntu_1204_x86.tar.gz --gzip
+tar --extract --directory ubuntu_1204_x86_64 --file build_ubuntu_1204_x86_64.tar.gz --gzip
+tar --extract --directory ubuntu_1404_x86 --file build_ubuntu_1404_x86.tar.gz --gzip
+tar --extract --directory ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64.tar.gz --gzip
+tar --extract --directory ubuntu_1604_x86 --file build_ubuntu_1604_x86.tar.gz --gzip
+tar --extract --directory ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64.tar.gz --gzip
+tar --extract --directory ubuntu_1610_x86 --file build_ubuntu_1610_x86.tar.gz --gzip
+tar --extract --directory ubuntu_1610_x86_64 --file build_ubuntu_1610_x86_64.tar.gz --gzip
+popd
+
+pushd build/net.zlib-zlib
+cmake -DCMAKE_INSTALL_PREFIX="" ../../zlib/installer
+make
+make package
+popd
