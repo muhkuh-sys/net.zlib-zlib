186bf6d603d8ed81786a024b70950901481176a8
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-10-31 14:13:38 +0100

    Do not install packages. Archive the zlib installation folder.

--- .lxc_build_ubuntu_1710_x64.sh
+++ .lxc_build_ubuntu_1710_x64.sh
@@ -47,20 +47,10 @@ lxc exec ${CONTAINER} -- bash -c 'rm -rf /tmp/build'
 lxc exec ${CONTAINER} -- bash -c 'mkdir /tmp/build'
 lxc exec ${CONTAINER} -- bash -c 'mount --bind /tmp/build /tmp/work/build'
 
-# Update the package list to prevent "not found" messages.
-lxc exec ${CONTAINER} -- bash -c 'apt-get update --assume-yes'
-
-# Install the project specific packages.
-lxc exec ${CONTAINER} -- bash -c 'apt-get install --assume-yes libacl1-dev lua5.1 lua-filesystem lua-expat lua51-mhash lua-sql-sqlite3'
-
 # Build the 64bit version.
 lxc exec ${CONTAINER} -- bash -c 'cd /tmp/work && bash .build03_linux.sh'
-lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1710_x86_64_lua5.1.tar.gz --gzip --directory /tmp/work/build/linux/lua5.1/install .'
-lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1710_x86_64_lua5.2.tar.gz --gzip --directory /tmp/work/build/linux/lua5.2/install .'
-lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1710_x86_64_lua5.3.tar.gz --gzip --directory /tmp/work/build/linux/lua5.3/install .'
-lxc file pull ${CONTAINER}/tmp/work/build/build_ubuntu_1710_x86_64_lua5.1.tar.gz build/
-lxc file pull ${CONTAINER}/tmp/work/build/build_ubuntu_1710_x86_64_lua5.2.tar.gz build/
-lxc file pull ${CONTAINER}/tmp/work/build/build_ubuntu_1710_x86_64_lua5.3.tar.gz build/
+lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1710_x86_64.tar.gz --gzip --directory /tmp/work/build/linux/zlib/install .'
+lxc file pull ${CONTAINER}/tmp/work/build/build_ubuntu_1710_x86_64.tar.gz build/
 
 # Stop and remove the container.
 lxc stop ${CONTAINER}
